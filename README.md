fb7e1d32ffde67bb533a1f10e922e95addd6d8b6
Bart van de Biezen <bart@bartvandebiezen.com>
2015-03-23 14:30:25 +0100

    New Less library
    Transition from leafo/lessphp to oyejorge/less.php

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Lessphp Plugin for Kirby 2
 
-This is a preprocessor for Less files. Built using the [lessphp library](https://github.com/leafo/lessphp) by Leaf Corcoran and [lessphp plugin for Kirby v1](https://github.com/janstieler/kirby-lessphp) by Jan Stieler. This plugin will automatically process Less files when changed.
+This is a preprocessor for Less files. Built using the [lessphp library](https://github.com/oyejorge/less.php) by Josh Schmidt and [lessphp plugin for Kirby 1](https://github.com/janstieler/kirby-lessphp) by Jan Stieler. This plugin will automatically process Less files when changed.
+
+## Installation
 
 1. Copy folder ‘lessphp’ inside ‘plugins’ to Kirby’s plugin folder.
 2. Copy file ‘less.php’ inside ‘snippet’ to Kirby’s snippet folder.
@@ -8,3 +10,7 @@ This is a preprocessor for Less files. Built using the [lessphp library](https:/
 4. Create a folder ‘less’ inside Kirby’s assets folder.
 5. Create a file ‘style.less’ and place it inside ‘assets/less’.
 6. Make sure 'css/style.css’ exists inside Kirby's assets folder.
+
+## Compatibility
+
+This Kirby 2 plugin is compatible with Less 1.7.
--- snippet/less.php
+++ snippet/less.php
@@ -9,15 +9,52 @@ $sourceFile = $root . "/assets/less/style.less";
 // Your final CSS file.
 $compiledFile = $root . "/assets/css/style.css";
 
-// Activate library.
-require "site/plugins/lessphp/lessc.inc.php";
-$less = new lessc;
+// Compile only when needed.
+if (filemtime($sourceFile) > filemtime($compiledFile)) {
 
-// Use compression provided by library.
-$less->setFormatter("compressed");
+	// Set compression provided by library.
+	$options = array('compress'=>true);
 
-// Compile and update your CSS file.
-$less->checkedCompile($sourceFile, $compiledFile);
+	// Activate library.
+	require "site/plugins/lessphp/Less.php";
+	$parser = new Less_Parser($options);
+
+	// Compile content in buffer
+	$parser->parseFile($sourceFile);
+	$buffer = $parser->getCss();
+
+	// Remove all CSS comments.
+	$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
+
+	// Remove lines and tabs.
+	$buffer = str_replace(array("\r\n", "\r", "\n", "\t"), '', $buffer);
+
+	// Remove unnecessary spaces.
+	$buffer = preg_replace('!\s+!', ' ', $buffer);
+	$buffer = str_replace(': ', ':', $buffer);
+	$buffer = str_replace('} ', '}', $buffer);
+	$buffer = str_replace('{ ', '{', $buffer);
+	$buffer = str_replace('; ', ';', $buffer);
+	$buffer = str_replace(', ', ',', $buffer);
+	$buffer = str_replace(' }', '}', $buffer);
+	$buffer = str_replace(' {', '{', $buffer);
+	$buffer = str_replace(' )', ')', $buffer);
+	$buffer = str_replace(' (', '(', $buffer);
+	$buffer = str_replace(') ', ')', $buffer);
+	$buffer = str_replace('( ', '(', $buffer);
+	$buffer = str_replace(' ;', ';', $buffer);
+	$buffer = str_replace(' ,', ',', $buffer);
+
+	// Fix spacing in media queries.
+	$buffer = str_replace('and(', 'and (', $buffer);
+	$buffer = str_replace(')and', ') and', $buffer);
+
+	// Remove last semi-colon within a CSS rule.
+	$buffer = str_replace(';}', '}', $buffer);
+
+	// Update your CSS file.
+	file_put_contents($compiledFile, $buffer);
+}
 
 ?>
 <link rel="stylesheet" href="<?php echo url('assets/css/style.css') ?>">
